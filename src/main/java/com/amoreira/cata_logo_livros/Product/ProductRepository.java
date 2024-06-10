58ff2a3284dcb55070f423699b088c92d649b3d7
victor_moreira <victormoreira@vivaldi.net>
2024-06-10 14:17:02 -0300

    terminar o sistem a Product

---
+++ src/main/java/com/amoreira/cata_logo_livros/Product/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.amoreira.cata_logo_livros.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
+}
---
+++ src/main/java/com/amoreira/cata_logo_livros/Product/ProductService.java
@@ -0,0 +1,10 @@
+package com.amoreira.cata_logo_livros.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductService {
+
+    @Autowired
+    private ProductRepository
+
+}
